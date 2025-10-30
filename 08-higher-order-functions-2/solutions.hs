eafa6c91ac6f29af7907815d38e1fc2842b7cb89
Filip Sodić <filip.sodic@fer.hr>
2025-10-30 22:27:57 +0100

    Update solutions for lecture 8

--- 08-higher-order-functions-2/solutions.hs
+++ 08-higher-order-functions-2/solutions.hs
@@ -53,3 +53,61 @@ isTitleCased = all (isUpper . head) . words
  -}
 getFilename :: String -> String
 getFilename = reverse . takeWhile (/= '/') . reverse
+
+-- Extra exercises
+
+filterWords :: [String] -> String -> String
+filterWords ws = unwords . filter (not . (`elem` ws)) . words
+
+studentsPassed :: [(String, Int)] -> [String]
+studentsPassed students = map fst $ filter didPass students
+  where
+    didPass = (>= halfOfMaxScore) . snd
+    halfOfMaxScore = maxScore `div` 2
+    maxScore = maximum $ map snd students
+
+applyPairs1 :: [(a -> b, a)] -> [b]
+applyPairs1 pairs = map (\(f, v) -> f v) pairs
+
+applyPairs2 :: [(a -> b, a)] -> [b]
+applyPairs2 = map (\(f, v) -> f v)
+
+applyPairs3 :: [(a -> b, a)] -> [b]
+applyPairs3 = map (uncurry ($))
+
+clamp :: (Ord a, Num a) => a -> a -> [a] -> [a]
+clamp lower upper = map (min upper) . map (max lower)
+
+(|>) :: a -> (a -> b) -> b
+value |> function = function value
+
+-- Alternative definition:
+-- (|>) = flip ($)
+
+simple :: Char
+simple = "Marko" |> head
+
+wordCount :: Int
+wordCount = "I am piping stuff like a pro!" |> words |> length
+
+oddSquareSum :: Int
+oddSquareSum = [1 .. 5] |> map (^ 2) |> filter odd |> sum
+
+initials3 :: String -> (String -> Bool) -> String -> String
+initials3 delimiter pred =
+  intercalate "." . map (map toUpper . take 1) . filter pred . words
+
+sortPairs :: (Ord b) => [(a, b)] -> [(a, b)]
+sortPairs = sortBy (comparing snd)
+
+-- Complexity?
+maxElemIndices :: (Ord a) => [a] -> [Int]
+maxElemIndices list = findIndices (== maximum list) list
+
+maxElemIndices' :: (Ord a) => [a] -> [Int]
+maxElemIndices' list = elemIndices (maximum list) list
+
+sumEven' :: (Num a) => [a] -> a
+sumEven' = foldl' sumIfEvenIndex 0 . zip [0 ..]
+  where
+    sumIfEvenIndex acc (idx, current) = if even idx then acc + current else acc
